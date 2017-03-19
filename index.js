f681213ce24ee63ac6ee9b8d0f2b1826668e95da
Stephen Bluck <stephen.bluck@photobox.com>
2017-03-19 20:16:41 +0000

    Added charge

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const firebase = require("firebase-admin")
 const cert = require('./privateKey.json')
 const pathMatch = require('path-match')
 const { parse } = require('url')
+const stripe = require('stripe')('sk_test_TrkNTx6ieFrACpGxHi0jHUmC')
 
 const { json, send } = micro
 
@@ -19,7 +20,6 @@ const server = micro(async (req, res) => {
 
   res.setHeader('Access-Control-Allow-Origin', '*')
 
-
   const route = pathMatch()
   const {pathname} = parse(req.url)
 
@@ -28,6 +28,9 @@ const server = micro(async (req, res) => {
 
   const createUserMatch = route('/user/create')
   const createUser = createUserMatch(pathname)
+
+  const createChargeMatch = route('/charge/create')
+  const createCharge = createChargeMatch(pathname)
 
   if(getUser && req.method === 'GET') {
     try {
@@ -48,6 +51,29 @@ const server = micro(async (req, res) => {
     try {
       return await auth.createUser({ email, password, displayName })
     } catch(e) {
+      return send(res, 400, e)
+    }
+  }
+
+ if(createCharge && req.method === 'POST') {
+    const { token, user, product } = await json(req)
+
+    try {
+      // Charge the user's card:
+      const charge = await stripe.charges.create({
+        description: product.excerpt,
+        amount: product.price,
+        currency: 'USD',
+        source: token.id,
+        statement_descriptor: `Koken Addons ${product.name}`
+      })
+
+      const db = firebase.database();
+      const ref = db.ref(`users/${user.uid}/products/${product.id}`)
+      const expiry = new Date().setFullYear(new Date().getFullYear() + 1)
+      return ref.set({ charge: charge.id, expiry, product: product.id})
+    } catch(e) {
+      console.log(e)
       return send(res, 400, e)
     }
   }
